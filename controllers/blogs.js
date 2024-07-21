5783977cf90c5cd6e976143486f00d68a2cee169
snaphuman <fhernandezn@gmail.com>
2024-07-20 19:11:03 -0500

    implement mongoDB queries for CRUD rest operations

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,23 +2,64 @@ const router = require('express').Router()
 const Blog = require('../models/blog')
 
 router.get('/', (req, res) => {
-
+    Blog.find({}).then(notes => {
+        res.json(notes)
+    })
 })
 
 router.get('/:id', (req, res, next) => {
-
+    Blog.findById(req.params.id)
+        .then((blog) => {
+            if(blog) {
+                res.json(blog)
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch((error) => next(error))
 })
 
 router.post('/', (req, res, next) => {
+    const { title, author, url, likes } = req.body;
 
+    const blog = new Blog({
+        title,
+        author,
+        url,
+        likes,
+    })
+
+    blog.save()
+        .then((saved) => {
+            res.json(saved)
+        })
+        .catch((error) => next(error))
 })
 
 router.delete('/:id', (req, res, next) => {
+    Blog.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch((error) => next(error))
 
 })
 
 router.put('/:id', (req, res, next) => {
+    const { title, author, url, likes } = req.body;
+    const blog = { title, author, url, likes }
 
+    Blog.findByIdAndUpdate(
+        req.params.id, 
+        blog, {
+            new: true,
+            runValidators: true,
+            context: 'query'
+        })
+        .then((updated) => {
+            res.send(updated)
+        })
+        .catch((error) => next(error))
 })
 
 module.exports = router
